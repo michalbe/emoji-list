d202e0a72db34c86429a3348c8c16dbd70764197
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 00:35:08 +0200

    [EMOJI-LIST] - :cake: comments in the code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 
-module.exports = fs.readFileSync('static/emoji.txt')
-                 .toString()
-                 .trim()
-                 .split('\n');
+module.exports = fs.readFileSync('static/emoji.txt') // load the text file with list
+                 .toString() // change the input file to string
+                 .trim() // remove whitechars at the end
+                 .split('\n'); // turn into an Array

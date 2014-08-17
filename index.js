e2a4255beb634d69ab1b3e6b1f3b1609fbc850db
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 01:01:34 +0200

    [EMOJI-LIST] - :trophy: list file path fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 
-module.exports = fs.readFileSync('static/emoji.txt') // load file with emojis
+module.exports = fs.readFileSync(__dirname + '/static/emoji.txt')
                  .toString() // change the input file to string
                  .trim() // remove whitechars at the end
                  .split('\n'); // turn into an Array
